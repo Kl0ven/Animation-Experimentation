ef3e92a9a68c468d3b11ddce537bce4b3a1b2a5e
Kloven <jloup.m@gmail.com>
2020-06-17 23:18:41 +0200

    add presets

--- 2_Maze/scripts.js
+++ 2_Maze/scripts.js
@@ -7,15 +7,43 @@ const ctx = canvas.getContext('2d');
 const styles = {
     backgroundColor: '#282828',
     wallsColor: '#67ff67',
-    visitedColor: '#4287f5',
+    visitedColor: '#282828',
     currentColor: '#166af2',
     lineWidth: 1
+};
+
+
+const presets = {
+    preset: 'Sober',
+    remembered: {
+        Sober: {
+            0: {
+                backgroundColor: '#282828',
+                wallsColor: '#282828',
+                visitedColor: '#413d3d',
+                currentColor: '#282828',
+                lineWidth: 1
+            }
+        },
+        BlueGreen: {
+            0: {
+                backgroundColor: '#282828',
+                wallsColor: '#67ff67',
+                visitedColor: '#4287f5',
+                currentColor: '#166af2',
+                lineWidth: 1
+            }
+
+        }
+    },
+    closed: false,
+    folders: {}
 };
 
 const settings = {
     restart: restart,
     cellSize: 10,
-    timeStep: 100
+    timeStep: 1
 };
 
 const width = window.innerWidth;
@@ -38,7 +66,8 @@ function init () {
 }
 
 function initGUI () {
-    const gui = new GUI();
+    const gui = new GUI({ load: presets});
+    gui.remember(styles);
     const colorFolder = gui.addFolder('Color');
     const settingsFolder = gui.addFolder('Settings');
 
@@ -49,9 +78,10 @@ function initGUI () {
     colorFolder.addColor(styles, 'wallsColor');
     colorFolder.addColor(styles, 'visitedColor');
     colorFolder.addColor(styles, 'currentColor');
+    colorFolder.add(styles, 'lineWidth', 0.5, 10).step(0.1);
 
     settingsFolder.add(settings, 'cellSize', 10, 100).step(1).onChange(restart);
-    settingsFolder.add(settings, 'timeStep', 10, 1000);
+    settingsFolder.add(settings, 'timeStep', 1, 100);
     gui.add(settings, 'restart');
 }
 
@@ -67,7 +97,7 @@ function restart () {
 
 
 (function () {
-    init();
     initGUI();
+    init();
     algorithm.run();
 })();
